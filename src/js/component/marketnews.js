7d8c95a6725e116ffea3b422d18bb6e9a9b09925
jgustavoj <gjime011@fiu.edu>
2021-03-19 23:54:44 +0000

    index

--- src/js/component/marketnews.js
+++ src/js/component/marketnews.js
@@ -14,54 +14,31 @@ export const MarketNews = () => {
 			console.log(response.body);
 		});
 	}, []);
-
-	// let result = getNews.filter(article => article.category == "business");
-	// let count = 0;
-	// let firstSix = result.filter(function(item) {
-	// 	if (count <= 6 && item > 0) {
-	// 		count++;
-	// 		return true;
-	// 	}
-	// 	return false;
-	// });
-	// console.log("$$", firstSix);
-
-	// return <></>;
-
-	//     const arr = [1,0,2,0,3,0,4,5,6,7,8,9,10,11,12,13,14];
-	// const filtered = arr.filter(function(item) {
-	//   if (this.count < 10 && item > 0) {
-	//     this.count++;
-	//     return true;
-	//   }
-	//   return false;
-	// }, {count: 0});
-
-	// console.log(filtered);
 	const result = getNews.filter(article => article.category == "business");
-	console.log("$$", result);
 	return (
 		<>
 			<div className="row">
 				{result.map((news, index) => {
-					return (
-						<article className="media pb-2" key={index}>
-							<figure className="media-left">
-								<p className="image is-32x32">
-									<img src={news.image} />
-								</p>
-							</figure>
-							<div className="media-content">
-								<div className="content">
-									<p>
-										<strong>{news.headline}</strong>
-										<br />
-										{news.summary}
+					if (index < 5) {
+						return (
+							<article className="media pb-2" key={index}>
+								<figure className="media-left">
+									<p className="image is-32x32">
+										<img src={news.image} />
 									</p>
+								</figure>
+								<div className="media-content">
+									<div className="content">
+										<p>
+											<strong>{news.headline}</strong>
+											<br />
+											{news.summary}
+										</p>
+									</div>
 								</div>
-							</div>
-						</article>
-					);
+							</article>
+						);
+					}
 				})}
 			</div>
 		</>
